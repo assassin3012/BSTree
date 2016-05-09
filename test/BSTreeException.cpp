efce2112cd03cad505afe6983d57ba78d722a4f8
assassin3012 <assassin3012@mail.ru>
2016-05-09 21:16:29 +0300

    Update BSTreeException.cpp

--- test/BSTreeException.cpp
+++ test/BSTreeException.cpp
@@ -1,1 +1,25 @@
+#pragma once
+#include <exception>
+using namespace std;
 
+class BSTreeException : public std::exception
+{
+public:
+	BSTreeException() : std::exception(invalid_argument("Unknown exception")) {};
+	BSTreeException(const char *_message) : std::exception(invalid_argument(_message)) {};
+};
+class EmptyException : public BSTreeException
+{
+public:
+	EmptyTree() : EmptyException("Tree is empty") {};
+};
+class InputException : public BSTreeException
+{
+public:
+	InputException() : TreeException("Error in input") {};
+};
+class InputFileException : public BSTreeException
+{
+public:
+	InputFileException() : TreeException("Error in input from file") {};
+};
