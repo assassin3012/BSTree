6a0d22ca5f08854cd2bc88f02e1005945cbe2854
assassin3012 <assassin3012@mail.ru>
2016-05-14 18:39:38 +0300

    Update BSTree.h

--- test/BSTree.h
+++ test/BSTree.h
@@ -26,7 +26,6 @@ struct Node
 {
 	Node<T>() : key(0), left(nullptr), right(nullptr) {};
 	Node<T>(const T &k) : key(k), left(nullptr), right(nullptr) {};
-	~Node<T>() { if (left) delete left; if (right) delete right; };
 
 	T key;
 	Node<T>* left;
@@ -40,22 +39,26 @@ class BSTree
 public:
 	BSTree<T>() : root(nullptr), _size(0) {};
 	BSTree<T>(const std::initializer_list<T> &);
-	~BSTree<T>() { delete root; };
-
-	Node<T>* search(const T &k);
+	~BSTree() { if (root!=nullptr) remove(root->key); }
+	Node<T>* search(const T &k) const;
+	Node<T>* next(Node<T>* node) const;
+	Node<T>* prev(Node<T>* node) const;
+	Node<T>* min(Node<T>* node) const;
+	Node<T>* max(Node<T>* node) const;
 	void insert(const T &k);
+	void deletetree(const T &k);
 	void remove(const T &k);
 	//auto begin() { return &root; };
 	//auto end() { return &root + sizeof(root)*_size+sizeof(_size); };
 	size_t size() { return _size; };
-	void printBSTree(std::ostream &os/*=std::cout*/);
+	void printBSTree(std::ostream &os/*=std::cout*/) const;
 	friend std::ostream & operator << <>(std::ostream &output, BSTree &);
 	friend std::istream & operator >> <>(std::istream &input, BSTree &);
 	friend std::fstream & operator << <>(std::fstream &file, BSTree<T> &);
 	friend std::fstream & operator >> <>(std::fstream &file, BSTree<T> &);
 private:
-	Node<T>* search(Node<T>* node, const T &k);
-	void printBSTree(Node<T>*, std::ostream &);
+	Node<T>* search(Node<T>* node, const T &k) const;
+	void printBSTree(Node<T>*, std::ostream &) const;
 	Node<T>* root;
 	size_t _size;
 };
