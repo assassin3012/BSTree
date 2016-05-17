40885c2b6125d5e55ffba7ed73234a370cba083a
assassin3012 <assassin3012@mail.ru>
2016-05-17 23:03:06 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -63,38 +63,38 @@ SCENARIO("BSTree remove", "[remove]")
 }
 SCENARIO("BSTree delete root without children", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8};
+	BSTree<int> Tree {8};
 	REQUIRE( Tree.remove(8) );
 	REQUIRE( Tree.size()==0 );
 }
 SCENARIO("BST delete root with one child", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8, 4};
+	BSTree<int> Tree {8, 4};
 	REQUIRE( Tree.remove(8) );
 	REQUIRE( Tree.size()==0 );
 	REQUIRE(Tree.search(4) != nullptr);
 }
 SCENARIO("BST delete root with children", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
+	BSTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
 	REQUIRE( Tree.remove(8) );
 	REQUIRE( Tree.size() == 7 );
 }
 SCENARIO("BST delete non root without children", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
+	BSTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
 	REQUIRE( Tree.remove(3) );
 	REQUIRE( Tree.size()==7 );
 }
 SCENARIO("BST delete non root with one child", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
+	BSTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
 	REQUIRE( Tree.remove(11) );
 	REQUIRE( Tree.size() == 7 );
 }
 SCENARIO("BST delete non root with one child", "[delete]") 
 {
-	BinarySearchTree<int> Tree {8, 4, 3, 10, 9, 13, 11, 12};
+	BSTree<int> Tree ={8, 4, 3, 10, 9, 13, 11, 12};
 	REQUIRE( Tree.remove(10) );
 	REQUIRE( Tree.size() == 7 );
 }
